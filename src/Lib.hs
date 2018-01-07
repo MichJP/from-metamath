17add4a1a81c0dacd0783c3a54e014580765ea0f
Michael Park <mjparksd@gmail.com>
2018-01-07 13:12:28 -0800

    List all meta tokens

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,12 +3,14 @@ module Lib
     ) where
 
 import System.IO
+import Data.List
 
 someFunc :: IO ()
 someFunc = do
   handle <- openFile "test/tutorial_example.mm" ReadMode 
   contents <- hGetContents handle
   putStrLn . stripComments $ contents
+  putStrLn . intercalate ", " $ collectMetaTokens contents
   hClose handle
 
 stripComments :: String -> String
@@ -18,6 +20,11 @@ stripComments (x:xs) = x : stripComments xs
 
 stripComments' :: String -> String
 stripComments' "" = error "No matching $) found!"
-stripComments' ('$':')': xs) = stripComments xs
+stripComments' ('$':')':xs) = stripComments xs
 stripComments' (_:xs) = stripComments' xs
+
+collectMetaTokens :: String -> [String]
+collectMetaTokens "" = []
+collectMetaTokens ('$':x:xs) = ('$':x:""):collectMetaTokens xs
+collectMetaTokens (_:xs) = collectMetaTokens xs
 
