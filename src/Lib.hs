1298068a74e7397a00e08d6c0db7b614da0cccf0
Michael Park <mjparksd@gmail.com>
2018-01-07 00:23:16 -0800

    Strip comments

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,22 @@ module Lib
     ( someFunc
     ) where
 
+import System.IO
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+  handle <- openFile "test/tutorial_example.mm" ReadMode 
+  contents <- hGetContents handle
+  putStrLn . stripComments $ contents
+  hClose handle
+
+stripComments :: String -> String
+stripComments "" = ""
+stripComments ('$':'(':xs) = stripComments' xs
+stripComments (x:xs) = x : stripComments xs
+
+stripComments' :: String -> String
+stripComments' "" = error "No matching $) found!"
+stripComments' ('$':')': xs) = stripComments xs
+stripComments' (_:xs) = stripComments' xs
+
